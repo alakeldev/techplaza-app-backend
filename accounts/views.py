86c9b1c4d13ee6d41b6702f1c25ca9230fc11446
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-07-28 18:35:35 +0200

    edit views.py 'create RegisterView() using GenericAPIView from rest_framework + call RegisterSerializer from serlizers.py + create post() fun inside the RegisterView and use Response and status from rest_framework to return the responses'

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer
 
 # Create your views here.
+
+class RegisterView(GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        user_data = request.data
+        serializer=self.serializer_class(data=user_data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            user=serializer.data
+            return Response({
+                'data':user,
+                'message':f'Thanks for your Registration 
+                {user.full_name}, a verified code has been sent to your Email.'
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, 
+                    status=status.HTTP_400_BAD_REQUEST)
