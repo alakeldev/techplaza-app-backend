7c6d02e505a04a23872be8a65522952da1aba879
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-07-28 12:44:26 +0200

    edit serializers.py inside account app 'create validate() to validate and checkon password fields + create create() method'

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -8,3 +8,20 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields=['full_name', 'email', 'password1', 'password2']
+
+    def validate(self, attrs):
+        password1 = attrs.get('password1', '')
+        password2 = attrs.get('password2', '')
+        if password1 != password2:
+            raise serializers.ValidationError("The password fields didn't match")
+        else:
+            return attrs
+    
+
+    def create(self, validated_data):
+        user=User.objects.create_user(
+            full_name=validated_data.get['full_name'],
+            email=validated_data.get['email'],
+            password1=validated_data.get['password1'],
+        )
+        return user
