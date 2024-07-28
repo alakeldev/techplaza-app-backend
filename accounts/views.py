d4aea7b8e60536b2cca360f3fee1b3b2cec1482d
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-07-28 19:35:58 +0200

    edit views.py 'import send_mail + random + string to generate random otp to send to the user email + edit settings 'added email backend django settings' + create accounts/urls.py and add path to the RegisterView + edit techplaza/urls.py to include the paths from accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,9 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegisterSerializer
+from django.core.mail import send_mail
+import random
+import string
 
 # Create your views here.
 
@@ -15,6 +18,17 @@ class RegisterView(GenericAPIView):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             user=serializer.data
+
+            otp = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+
+            send_mail(
+                'OTP for Registration Verification',
+                f'Your OTP for registration verification is {otp}.',
+                'techplaza1@hotmail.com',
+                [user['email']],
+                fail_silently=False,
+            )
+
             return Response({
                 'data':user,
                 'message':f'Thanks for your Registration 
--- techplaza_api/settings.py
+++ techplaza_api/settings.py
@@ -144,6 +144,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp-mail.outlook.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_USE_SSL = False
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
