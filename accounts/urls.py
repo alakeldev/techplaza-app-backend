9786082cbce62325be359dc3eefae60fe1a77c98
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-07-30 21:29:33 +0200

    edit urls.py accounts app 'call passwordresetview / confirmpasswordresetview / newpasswordview and set path for each one'

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import RegisterView, VerifyEmail, LoginView
+from .views import RegisterView, VerifyEmail, LoginView, PasswordResetView, ConfirmPasswordResetView, NewPasswordView
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('email-verify/', VerifyEmail.as_view(), name='email-verify'),
     path('login/', LoginView.as_view(), name='login'),
+    path('password-reset/', PasswordResetView.as_view(), name='password-reset'),
+    path('confirm-password-reset/<uidb64>/<token>/', ConfirmPasswordResetView.as_view(), name='confirm-password-reset'),
+    path('new-password/', NewPasswordView.as_view(), name='new-password'),
 ]
