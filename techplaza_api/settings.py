9ffdb4b89c2f562df838d9801068d0dc30819f1a
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-05 21:17:27 +0200

    create weather app and all its files + its urls.py + edit main project urls.py'include the urls of weather app' + edit settings.py 'create variable to store the API weather key inside it and call it from the env file + call the weather app inside installed apps inside settings.py'

--- techplaza_api/settings.py
+++ techplaza_api/settings.py
@@ -27,6 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
+WEATHER_API_KEY = os.environ.get('WEATHER_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
@@ -52,6 +53,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
 
     'accounts',
+    'weather',
 ]
 
 MIDDLEWARE = [
