8d42362a0929c5bfbe692b434203567e0c5266f6
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-06 18:21:01 +0200

    edit models.py tasks app 'create task model' + added migration_file_0001 tasks app

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class task(models.Model):
+    task_title = models.CharField(max_length=100)
+    task_description = models.CharField(max_length=250)
+    is_done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.task_title
