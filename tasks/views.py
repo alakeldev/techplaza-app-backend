a16ca076c6d76f3059058bb40c5d87c1e06cc7f3
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-08 21:02:56 +0200

    edit views.py tasks app 'import IsAuthenticated from DRF permissions + edit TaskView() 'create get_queryset & preform_create funs''

--- tasks/views.py
+++ tasks/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
 class TaskView(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
-    queryset = Task.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
