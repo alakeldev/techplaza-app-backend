bc1da210393966efe30a5e07525a2c25c97ddaa7
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-08 21:58:01 +0200

    edit urls.py accounts app 'import tokenobatinpairview from DRF-simplejwt.views + set its path'

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from .views import RegisterView, VerifyEmail, LoginView, PasswordResetView, ConfirmPasswordResetView, NewPasswordView, LogoutView ,UpdateInformationView, DeleteAccountView, DashboardView
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
     path('email-verify/', VerifyEmail.as_view(), name='email-verify'),
     path('login/', LoginView.as_view(), name='login'),
     path('dashboard-user/', DashboardView.as_view(), name='dashboard-user'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('password-reset/', PasswordResetView.as_view(), name='password-reset'),
     path('confirm-password-reset/<uidb64>/<token>/', ConfirmPasswordResetView.as_view(), name='confirm-password-reset'),
     path('new-password/', NewPasswordView.as_view(), name='new-password'),
