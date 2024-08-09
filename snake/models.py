a1bde65cd06a945260d852197c80f27e7eeec9e0
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-09 14:00:47 +0200

    edit models.py snake app/game 'import User model from accounts app + create HightScore model'

--- snake/models.py
+++ snake/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+
+class HighScore(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.user.email} - {self.score}'
