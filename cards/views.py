0f84f15d6818c1e2a17a3acff535894db7565c53
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-15 11:22:48 +0200

    edit views.py cards app 'added CardViewSet, authentication and user-specific queryset'

--- cards/views.py
+++ cards/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Card
+from .serializers import CardSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+class CardViewSet(viewsets.ModelViewSet):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Card.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
