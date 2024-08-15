1296a0f8019f5a63671ea5af6453e49ff706d4bc
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-15 11:14:31 +0200

    edit serializers.py cards app 'added CardSerializer using card model with user email and name'

---
+++ cards/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Card
+
+class CardSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(source='user.email', read_only=True)
+    name = serializers.CharField(source='user.full_name', read_only=True)
+
+    class Meta:
+        model = Card
+        fields = ['id', 'email', 'name', 'phone_number', 'profession', 'description', 'country', 'photo']
