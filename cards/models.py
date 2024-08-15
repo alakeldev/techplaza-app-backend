e8a01db652c5459a635db629d117d3acd585b39f
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-15 11:08:36 +0200

    edit models.py 'added card model with its fields include cloudinary field' + added migrations file 0001 related to cards app

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Card(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20)
+    profession = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    country = models.CharField(max_length=30)
+    photo = CloudinaryField('image', folder='members_cards_images', null=True, blank=True)
+
+    def __str__(self):
+        return self.user.email
