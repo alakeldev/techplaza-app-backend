d819ba20003b7d53d4b35fe8c002b774adf5b67f
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-16 19:20:04 +0200

    edit settings.py added Default permission classes inside Rest_framework settings to resolve a bug on console

--- techplaza_api/settings.py
+++ techplaza_api/settings.py
@@ -119,7 +119,10 @@ DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 SIMPLE_JWT = {
