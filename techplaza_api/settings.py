4c5c486f301fa90dde88cbdd2d705025233e095d
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-18 13:21:33 +0200

    edit settings.py 'edit & add some SIMPLE_JWT settings'

--- techplaza_api/settings.py
+++ techplaza_api/settings.py
@@ -123,11 +123,13 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
+    'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'SIGNING_KEY': SECRET_KEY,
 }
 
 # Password validation
