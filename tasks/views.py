66c7aa8ef7679188d0e08c5627d76cb181664c5b
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-18 22:13:12 +0200

    edit views.py tasks app 'remove all the custom of create, update ,delete action, focus to work with standard implemenation for DRF standard way'

--- tasks/views.py
+++ tasks/views.py
@@ -3,45 +3,12 @@ from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
-import logging
 
 # Create your views here.
 
-logger = logging.getLogger(__name__)
 class TaskView(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        try:
-            logger.debug(f"Fetching tasks for user: {self.request.user}")
-            return Task.objects.filter(user=self.request.user)
-        except Exception as e:
-            logger.error(f"Error fetching tasks: {e}")
-            return Response({"error": "Error fetching tasks"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def perform_create(self, serializer):
-        try:
-            logger.debug(f"Creating task for user: {self.request.user}")
-            serializer.save(user=self.request.user)
-        except Exception as e:
-            logger.error(f"Error creating task: {e}")
-            return Response({"error": "Error creating task"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def perform_update(self, serializer):
-        try:
-            logger.debug(f"Updating task for user: {self.request.user}")
-            serializer.save(user=self.request.user)
-        except Exception as e:
-            logger.error(f"Error updating task: {e}")
-            return Response({"error": "Error updating task"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def perform_destroy(self, instance):
-        try:
-            logger.debug(f"Deleting task for user: {self.request.user}")
-            instance.delete()
-        except Exception as e:
-            logger.error(f"Error deleting task: {e}")
-            return Response({"error": "Error deleting task"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Task.objects.filter(user=self.request.user)
