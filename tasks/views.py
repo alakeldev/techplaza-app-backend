37550fa4562b10b89b2c096a6dd3710e221ee1e0
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-18 22:58:29 +0200

    edit views.py tasks app 'added preform_create fun again to TaskView after trace an error on the heroku logs with creating new tasks on database'

--- tasks/views.py
+++ tasks/views.py
@@ -12,3 +12,6 @@ class TaskView(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
