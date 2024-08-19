b32367caf2672f2c2b35b278be76bdba57d6d523
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-19 02:49:42 +0200

    edit settings.py 'edit JWT-SIMPLE settings testing change' + edit urls.py accounts and urls.py project 'move the token views from accounts app to the main project urls.py testing change'

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 from .views import RegisterView, VerifyEmail, LoginView, PasswordResetView, ConfirmPasswordResetView, NewPasswordView, LogoutView ,UpdateInformationView, DeleteAccountView
-from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
     path('email-verify/', VerifyEmail.as_view(), name='email-verify'),
     path('login/', LoginView.as_view(), name='login'),
