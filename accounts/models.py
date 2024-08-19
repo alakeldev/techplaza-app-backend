7510133f76c0330777273d6553affe4f103df64e
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-19 11:45:04 +0200

    edit models.py accounts app 'edit user_token fun'

--- accounts/models.py
+++ accounts/models.py
@@ -32,5 +32,5 @@ class User(AbstractBaseUser, PermissionsMixin):
         refresh=RefreshToken.for_user(self)
         return {
             'refresh_token': str(refresh),
-            'token': str(refresh.access),
+            'token': str(refresh.token),
         }
