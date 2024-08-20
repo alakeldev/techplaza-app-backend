8d0931cff2339e6b0a82a529a5f1ebc0241c552f
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-20 22:05:00 +0200

    edit admin.py accounts app 'create UserAdmin class and register it to admin panel'

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import User
 # Register your models here.
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'full_name', 'is_verified', 'date_joined')
+    list_filter = ('is_verified', 'date_joined')
+    search_fields = ('email', 'full_name')
+    ordering = ('-date_joined',)
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
