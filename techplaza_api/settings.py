c047a779252815711974eaa95585b0ac048e21d0
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-08-28 15:29:20 +0200

    create exceptions.py in accounts app + edit settings.py edit Rest_framework settings

--- techplaza_api/settings.py
+++ techplaza_api/settings.py
@@ -128,6 +128,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
     ),
+    'EXCEPTION_HANDLER': 'accounts.exceptions.custom_exception_handler',
 }
 
 SIMPLE_JWT = {
