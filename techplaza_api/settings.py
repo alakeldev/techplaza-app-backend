348d5a22805620a78572d7b37f7b598314b72f81
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-10-02 20:22:38 +0200

    edit settings.py 'back email settings to outlook trying to fix the issues with smtp server'

--- techplaza_api/settings.py
+++ techplaza_api/settings.py
@@ -187,11 +187,12 @@ USE_I18N = True
 USE_TZ = True
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.mail.yahoo.com"
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
+EMAIL_HOST = "smtp-mail.outlook.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_USE_SSL = False
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 # Static files (CSS, JavaScript, Images)
