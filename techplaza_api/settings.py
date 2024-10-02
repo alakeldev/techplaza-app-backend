e0551d9107db70c87cbce81a84d12a6d5f044a84
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-10-02 19:43:30 +0200

    edit settings.py 'fixed the issues on the live version for SMTP sever'

--- techplaza_api/settings.py
+++ techplaza_api/settings.py
@@ -188,9 +188,8 @@ USE_TZ = True
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.mail.yahoo.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
