bb279cf708b605c384fe614e580c8d986c8ba808
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-10-02 18:54:18 +0200

    edit settings.py 'edit the Email_Host trying to fix issues with outlook/hotmail smtp'

--- techplaza_api/settings.py
+++ techplaza_api/settings.py
@@ -187,7 +187,7 @@ USE_I18N = True
 USE_TZ = True
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.office365.com"
+EMAIL_HOST = "smtp-mail.outlook.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
